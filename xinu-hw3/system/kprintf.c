2412238b2dd05889982724cf235acdd85869dc3d
blackhawks28 <49330074+blackhawks28@users.noreply.github.com>
2021-02-14 18:27:21 -0600

    Add files via upload

---
+++ xinu-hw3/system/kprintf.c
@@ -0,0 +1,117 @@
+/**
+ * @file kprintf.c
+ */
+
+/* Embedded Xinu, Copyright (C) 2009, 2013.  All rights reserved. */
+
+#include <xinu.h>
+
+#define UNGETMAX 10             /* Can un-get at most 10 characters. */
+
+static unsigned char ungetArray[UNGETMAX];
+
+/**
+ * Synchronously read a character from a UART.  This blocks until a character is
+ * available.  The interrupt handler is not used.
+ *
+ * @return
+ *      The character read from the UART as an <code>unsigned char</code> cast
+ *      to an <code>int</code>.
+ */
+syscall kgetc(void)
+{
+    volatile struct pl011_uart_csreg *regptr;
+
+    /* Pointer to the UART control and status registers.  */
+    regptr = (struct pl011_uart_csreg *)0x3F201000;
+
+    // TODO: First, check the unget buffer for a character.
+    //       Otherwise, check UART flags register, and
+    //       once the receiver is not empty, get character c.
+
+    return SYSERR;
+}
+
+/**
+ * kcheckc - check to see if a character is available.
+ * @return true if a character is available, false otherwise.
+ */
+syscall kcheckc(void)
+{
+    volatile struct pl011_uart_csreg *regptr;
+    regptr = (struct pl011_uart_csreg *)0x3F201000;
+
+    // TODO: Check the unget buffer and the UART for characters.
+	
+	
+
+    return SYSERR;
+}
+
+/**
+ * kungetc - put a serial character "back" into a local buffer.
+ * @param c character to unget.
+ * @return c on success, SYSERR on failure.
+ */
+syscall kungetc(unsigned char c)
+{
+    // TODO: Check for room in unget buffer, put the character in or discard.
+	
+
+    return SYSERR;
+}
+
+
+/**
+ * Synchronously write a character to a UART.  This blocks until the character
+ * has been written to the hardware.  The interrupt handler is not used.
+ *
+ * @param c
+ *      The character to write.
+ *
+ * @return
+ *      The character written to the UART as an <code>unsigned char</code> cast
+ *      to an <code>int</code>.
+ */
+syscall kputc(uchar c)
+{
+    volatile struct pl011_uart_csreg *regptr;
+
+    /* Pointer to the UART control and status registers.  */
+    regptr = (struct pl011_uart_csreg *)0x3F201000;
+
+    // TODO: Check UART flags register.
+    //       Once the Transmitter FIFO is not full, send character c.
+	
+	while(regptr(PL011_FR_RXFF)){
+		PL011_REGS_BAS7
+		
+	}
+	
+
+    return SYSERR;
+}
+
+/**
+ * kernel printf: formatted, synchronous output to SERIAL0.
+ *
+ * @param format
+ *      The format string.  Not all standard format specifiers are supported by
+ *      this implementation.  See _doprnt() for a description of supported
+ *      conversion specifications.
+ * @param ...
+ *      Arguments matching those in the format string.
+ *
+ * @return
+ *      The number of characters written.
+ */
+syscall kprintf(const char *format, ...)
+{
+    int retval;
+    va_list ap;
+
+    va_start(ap, format);
+    retval = _doprnt(format, ap, (int (*)(int, int))kputc, 0);
+    va_end(ap);
+    return retval;
+}
---
+++ xinu-hw3/system/testcases.c
@@ -0,0 +1,44 @@
+/**
+ * @file testcases.c
+ * @provides testcases
+ *
+ * $Id: testcases.c 175 2008-01-30 01:18:27Z brylow $
+ *
+ * Modified by:
+ *
+ * and
+ *
+ */
+/* Embedded XINU, Copyright (C) 2007.  All rights reserved. */
+
+#include <xinu.h>
+
+devcall putc(int dev, char c)
+{
+    return 0;
+}
+
+
+/**
+ * testcases - called after initialization completes to test things.
+ */
+void testcases(void)
+{
+    int c;
+
+    kprintf("===TEST BEGIN===\r\n");
+
+    c = kgetc();
+    switch (c)
+    {
+
+        // TODO: Test your operating system!
+
+    default:
+        kprintf("Hello Xinu World!\r\n");
+    }
+
+
+    kprintf("\r\n===TEST END===\r\n");
+    return;
+}
